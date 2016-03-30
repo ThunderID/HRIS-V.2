bed8fe102a754ff51127b01894a7098bb20e1923
chelsymooy <chelsymooy1108@gmail.com>
2016-03-30 13:19:34 +0700

    Working on org card (image issue)

---
+++ resources/views/desktop_v2/components/card.blade.php
@@ -0,0 +1,23 @@
+<div class="row">
+	<div class="col-sm-2 padding-right-0">
+		<div class="background-white height-120">
+			<img src="http://business-peoples.com/material/259.jpg" class="img-fluid">
+		</div>	
+	</div>	
+	<div class="col-sm-10 padding-left-5">
+		<div class=" background-white padding-15 height-120">
+			<p class="text-uppercase font-25">
+				{{$card_content['name']}}
+			</p>
+			<p class="padding-top-30 margin-bottom-0 padding-bottom-0 font-18">
+				<a class="link-black">
+					<i class="ion-android-create"></i> Ubah
+				</a>
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				<a class="link-black">
+					<i class="ion-android-delete"></i> Hapus
+				</a>
+			</p>
+		</div>
+	</div>
+</div>	
--- resources/views/desktop_v2/pages/org/index.blade.php
+++ resources/views/desktop_v2/pages/org/index.blade.php
@@ -10,7 +10,29 @@
 				@include('desktop_v2.components.grand_search_box')
 			</div>
 		</div>
+
+		<div class="clearfix">&nbsp;</div>
+		<div class="clearfix">&nbsp;</div>
+
 		<!-- End of Search Box -->
+		<!-- Content -->
+		<?php $initial = '0'; ?>
+		<div class="row">
+			@foreach($page_datas->datas['data'] as $key => $dt)
+				@if($initial!=strtoupper($dt['name'][0]))
+					<div class="col-sm-12">
+						<h3 class="text-uppercase">{{strtoupper($dt['name'][0])}}</h3>
+					</div>
+				@endif
+
+			<div class="col-sm-6">
+				@include('desktop_v2.components.card', ['card_content' => $dt])
+			</div>
+			
+			<?php $initial = strtoupper($dt['name'][0]);?>
+			@endforeach
+		</div>
+		<!-- End of Content -->
 	</div>
 </div>
 @stop
