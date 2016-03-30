212bf015c1339ddc2c3481cd598535fcb10e2a5e
chelsymooy <chelsymooy1108@gmail.com>
2016-03-30 12:20:02 +0700

    Search box

---
+++ resources/views/desktop_v2/components/grand_search_box.blade.php
@@ -0,0 +1,6 @@
+<form>
+	<div class="container-search-box">
+		<input type="search" id="search" placeholder="Cari Perusahaan" />
+		<button class="icon"><i class="ion-android-search font-size-25"></i></button>
+	</div>
+</form>
