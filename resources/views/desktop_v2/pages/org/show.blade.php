5031984e46456dfc7208f83cc2d34d732bde685a
chelsymooy <chelsymooy1108@gmail.com>
2016-03-31 11:33:36 +0700

    Enhance show

--- resources/views/desktop_v2/pages/org/show.blade.php
+++ resources/views/desktop_v2/pages/org/show.blade.php
@@ -63,4 +63,61 @@
 	</div>
 </div>
 <!-- End of Stat -->
+<div class="clearfix">&nbsp;</div>
+<!-- Report and Notif -->
+<div class="row padding-top-15 margin-left-0 margin-right-0 font-black">
+	<div class="col-sm-6">
+		<div class="card">
+			<div class="card-header background-white font-size-18">
+				Pengambilan Cuti Bulan ini
+			</div>
+			<div class="card-block background-white">
+				<blockquote class="card-blockquote">
+					<div class="font-size-25 font-red"><i><small>Silahkan upgrade plan Anda</small></i></div>
+				</blockquote>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-6">
+		<div class="card">
+			<div class="card-header background-yellow font-size-18">
+				Kontrak Kerja yang Akan Kadaluarsa
+			</div>
+			<div class="card-block background-yellow">
+				<blockquote class="card-blockquote">
+					<div class="font-size-25 font-red"><i><small>Silahkan upgrade plan Anda</small></i></div>
+				</blockquote>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- End of Report and Notif -->
+<!-- Chart and Calendar -->
+<div class="row padding-top-15 margin-left-0 margin-right-0 font-black">
+	<div class="col-sm-6">
+		<div class="card">
+			<div class="card-header background-white font-size-18">
+				Grafik Kinerja Karyawan
+			</div>
+			<div class="card-block background-white">
+				<blockquote class="card-blockquote">
+					<div class="font-size-25 font-red"><i><small>Silahkan upgrade plan Anda</small></i></div>
+				</blockquote>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-6">
+		<div class="card">
+			<div class="card-header background-white font-size-18">
+				Kalender Kerja
+			</div>
+			<div class="card-block background-white">
+				<blockquote class="card-blockquote">
+					<div class="font-size-25 font-red"><i><small>Silahkan upgrade plan Anda</small></i></div>
+				</blockquote>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- End of Chart and Calendar -->
 @stop
