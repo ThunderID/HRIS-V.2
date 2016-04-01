f8ff28303f8d76974864758fa490d8467078f5c1
chelsymooy <chelsymooy1108@gmail.com>
2016-04-01 07:25:15 +0700

    Fixed policy

---
+++ app/Http/Controllers/Org/PolicyController.php
@@ -0,0 +1,86 @@
+<?php namespace App\Http\Controllers\Org;
+
+use App\API\Connectors\APIOrg;
+
+use App\Http\Controllers\BaseController;
+use App\Http\Controllers\Helper\SortList;
+use Input, Route;
+
+/**
+ * { OrgController Class }
+ * @author Chelsy
+ * 
+ * public functions :
+ * 1. index()                           : public function display index org
+ * 
+ */
+
+class PolicyController extends BaseController 
+{
+	//init 
+	protected $view_source_root                     = 'pages.policy';
+	
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->page_attributes->page_title             = 'Kebijakan';
+		$this->page_attributes->breadcrumb             =    [
+															];        
+	}
+
+	/**
+	 * { index }
+	 *
+	 * @param     
+	 *
+	 * @return
+	 */
+	public function index($org_id = 0)
+	{
+		//1. validate
+		if(is_null($org_id))
+		{
+			App::abort(403, 'Id Organisasi tidak ada');
+		}
+
+		//2. get data
+		$SortList                                   = new SortList;
+
+		$this->page_attributes->page_subtitle       = 'index';
+		//dummy
+		$this->page_attributes->filters             =   [
+														];
+		//dummy
+		$this->page_attributes->sorts               =   [
+														];       
+
+		//2. get data parameter
+		$data_parameter                             = $this->setPageDataParameter();
+
+		//3. get data
+		$search                                     = array_merge(
+															['name' => $data_parameter['search']],
+															$data_parameter['filter']
+														);
+		$APIOrg										= new APIOrg;
+		$organisation								= $APIOrg->getShow($org_id);
+
+		//4. set page datas
+		$this->page_datas->datas['id']				= $org_id;
+
+		//5. generate view
+		$this->page_attributes->breadcrumb          = array_merge(
+															$this->page_attributes->breadcrumb,
+															[
+																$organisation['data']['name'] => route('org.show', ['id' => $org_id]),
+																'Kebijakan' => route('policy.index', ['org_id' => $org_id]),
+															]
+														);
+
+		$view_source                                = $this->view_source_root . '.index';
+		$route_source                               = route(Route::CurrentRouteName());
+
+		return $this->generateView($view_source, $route_source);
+	}
+}
---
+++ resources/views/desktop_v2/pages/policy/index.blade.php
@@ -0,0 +1,19 @@
+@extends('desktop_v2.page_templates.wireframe_with_menu')
+
+@section('content')
+
+<!-- Branch Index -->
+<div class="row">
+	<div class="col-md-12 text-xs-center" style="padding-top:calc(10% + 50px);">
+			<p class="font-size-25 font-red">Silahkan update plan Anda</p>
+	</div>
+</div>
+
+<!-- End of Branch Index -->
+@stop
+
+@section('js')
+	<script type="text/javascript">
+		hris_slimscroll.init();
+	</script>
+@stop
