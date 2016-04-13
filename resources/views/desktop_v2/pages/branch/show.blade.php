51c51b1d7c3c7d029f698139d90c2cc69fb7ecaa
budi7 <budi-purnomo@outlook.com>
2016-04-13 16:47:35 +0700

    Working chart

--- resources/views/desktop_v2/pages/branch/show.blade.php
+++ resources/views/desktop_v2/pages/branch/show.blade.php
@@ -3,6 +3,126 @@
 @section('content')
 
 <!-- Branch Index -->
+<style>
+/** {margin: 0; padding: 0;}*/
+
+.tree ul {
+	padding-top: 20px; position: relative;
+	
+	transition: all 0.5s;
+	-webkit-transition: all 0.5s;
+	-moz-transition: all 0.5s;
+}
+
+.tree li {
+	float: left; text-align: center;
+	list-style-type: none;
+	position: relative;
+	padding: 20px 5px 0 5px;
+	
+	transition: all 0.5s;
+	-webkit-transition: all 0.5s;
+	-moz-transition: all 0.5s;
+}
+
+/*We will use ::before and ::after to draw the connectors*/
+
+.tree li::before, .tree li::after{
+	content: '';
+	position: absolute; top: 0; right: 50%;
+	border-top: 1px solid #ccc;
+	width: 50%; height: 20px;
+}
+.tree li::after{
+	right: auto; left: 50%;
+	border-left: 1px solid #ccc;
+}
+
+/*We need to remove left-right connectors from elements without 
+any siblings*/
+.tree li:only-child::after, .tree li:only-child::before {
+	display: none;
+}
+
+/*Remove space from the top of single children*/
+.tree li:only-child{ padding-top: 0;}
+
+/*Remove left connector from first child and 
+right connector from last child*/
+.tree li:first-child::before, .tree li:last-child::after{
+	border: 0 none;
+}
+/*Adding back the vertical connector to the last nodes*/
+.tree li:last-child::before{
+	border-right: 1px solid #ccc;
+	border-radius: 0 0 0;
+	-webkit-border-radius: 0 0 0;
+	-moz-border-radius: 0 0 0;
+}
+.tree li:first-child::after{
+	border-radius: 0 0 0 0;
+	-webkit-border-radius: 0 0 0 0;
+	-moz-border-radius: 0 0 0 0;
+}
+
+/*Time to add downward connectors from parents*/
+.tree ul ul::before{
+	content: '';
+	position: absolute; top: 0; left: 50%;
+	border-left: 1px solid #ccc;
+	width: 0; height: 20px;
+}
+
+.tree li div{
+	border: 1px solid #ccc;
+	padding: 5px 10px;
+	text-decoration: none;
+	color: #666;
+	font-family: arial, verdana, tahoma;
+	font-size: 11px;
+	display: inline-block;
+	
+	border-radius: 0px;
+	-webkit-border-radius: 0px;
+	-moz-border-radius: 0px;
+	
+	transition: all 0.5s;
+	-webkit-transition: all 0.5s;
+	-moz-transition: all 0.5s;
+}
+
+/*Time for some hover effects*/
+/*We will apply the hover effect the the lineage of the element also*/
+.tree li div:hover, .tree li div:hover+ul li div {
+	background: #c8e4f8; color: #000; border: 1px solid #94a0b4;
+}
+/*Connector styles on hover*/
+.tree li div:hover+ul li::after, 
+.tree li div:hover+ul li::before, 
+.tree li div:hover+ul::before, 
+.tree li div:hover+ul ul::before{
+	border-color:  #94a0b4;
+}
+
+.FixedHeightContainer
+{
+  float:right;
+  height: 250px;
+  width:250px; 
+  padding:3px; 
+    background:#f00;
+}
+.Content
+{
+  height:224px;
+  width:224px;
+   overflow-x:scroll;
+   overflow-y:scroll;
+    background:#fff;
+}
+
+</style>
+
 <div class="row">
 	<div class="col-md-3">
 		@include('desktop_v2.components.grand_search_box', ['search_name' => 'q', 'search_placeholder' => 'Cari Cabang', 'background_search_box' => 'background-light-blue', 'font_search_box' => 'font-dark-blue'])
@@ -51,19 +171,88 @@
 						
 						@include('desktop_v2.components.alert_box')
 					</div>
-					
-					@forelse($page_datas->datas['charts'] as $key => $dt)
-
-					<div class="col-md-3 col-sm-12">
-						@include('desktop_v2.components.node', ['card_content' => $dt])
-					</div>
-					
-					@empty
-						<div class="col-md-12 col-sm-12">
-							<p class="background-white padding-15">Tidak ada data struktur organisasi</p>
-						</div>
-					@endforelse
 				</div>
+				<div class="row text-xs-center">
+					<div class="col-md-12">
+						<div style="border-bottom: black 0.2px solid; border-left: black 0.2px solid; overflow-x: auto; overflow-y: auto; margin-bottom: 10px; height: 400px; border-top: black 0.2px solid; border-right: black 0.2px solid" class="tree">
+							<div id="frame-chart">
+								@forelse($page_datas->datas['charts'] as $key => $dt)
+									<?php
+										function breakChilds($child)
+										{	
+											$width = 0;				
+											foreach ($child as $key => $value) 
+											{
+												$width = $width + 158;
+												echo("
+													<li>
+														<div style='padding=0px;'>
+															<p style='margin-bottom:5px!important;font-size:14px;'>" 
+																. $value['name'] . 
+															"</p>
+															<a href='#' style='width:100px!important;text-align:center;'>
+																Tambah
+															</a>
+															&nbsp;
+															<a href='#' style='width:100px!important;text-align:center;'>
+																Edit
+															</a>
+															&nbsp;
+															<a href='#' style='width:100px!important;text-align:center;'>
+																Hapus
+															</a>									
+														</div>
+													");
+
+												if(count($value['childs']) != 0)
+												{
+													echo("<ul>");
+													$width = $width + breakChilds($value['childs']);
+													echo("</ul>");
+
+													$width = $width - 158;
+												}
+												else
+												{
+													echo("</li>");
+												}
+											}
+
+											return $width;
+										}
+
+										echo("
+											<ul>
+												<li>
+													<div style='padding=0px;'>
+														<p style='margin-bottom:5px!important;font-size:14px;'>" 
+															. $dt['name'] . 
+														"</p>
+														<a href='#' style='width:100px!important;text-align:center;'>
+															Tambah
+														</a>
+														&nbsp;
+														<a href='#' style='width:100px!important;text-align:center;'>
+															Edit
+														</a>
+														&nbsp;
+														<a href='#' style='width:100px!important;text-align:center;'>
+															Hapus
+														</a>									
+													</div>
+												<ul>
+											");
+										$width =  breakChilds($dt['childs']);
+										echo("</ul></li></ul>");
+									?>
+								@empty
+									<p class="background-white padding-15">Tidak ada data struktur organisasi</p>
+								@endforelse
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	</div>
@@ -88,7 +277,15 @@
 
 @section('js')
 	<script type="text/javascript">
-		hris_slimscroll.init();
+		function setChartWidth(new_width) {
+		    $("#frame-chart").width(new_width);
+		}
+
+		$( document ).ready(function() {
+		    setChartWidth({{$width}});
+		});
+
+		// hris_slimscroll.init(); 
 		hris_modal_delete.init();
 		hris_modal_chart_edit.init();
 	</script>
