185f8d3f468db95a493ac1d032d058235bbb860c
budi7 <budi-purnomo@outlook.com>
2016-04-15 15:42:08 +0700

    working employee import

---
+++ resources/views/desktop_v2/components/modals/modal_employee_import.php
@@ -0,0 +1,28 @@
+<!-- Modal -->
+<div id="{{$modal_id}}" class="modal fade  padding-top-58" role="dialog">
+	<div class="modal-dialog">
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><i class="ion-android-close"></i></button>
+				<div class="font-18 modal_title text-uppercase"></div>
+			</div>
+			<div class="modal-body">
+				<!-- <p class="danger text-center">Error apa gitu</p> -->
+
+					<div class="form-group">
+						<div class="font-14 modal_effect"></div>
+					</div>
+					 <div class="form-group">
+
+					</div>
+					</br>
+					<div class="form-group text-xs-right">
+						<button type="button" class="button-gray" tabindex="2" data-dismiss="modal">Batal</button>
+						<button type="submit" class="button-red" tabindex="1">Hapus</button>
+					</div>    
+
+			</div>
+		</div>
+	</div>
+</div>
